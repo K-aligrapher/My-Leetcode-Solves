81dc1fd7f6f1c8ac29167324db1884fd13287e96
P Koti Darshan <152671869+K-aligrapher@users.noreply.github.com>
2026-08-11 22:53:28 +0530

    Time: 0 ms (100%), Space: 9 MB (50.89%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -6,7 +6,6 @@ public:
         vector<int> row(rowIndex + 1, 1);
         
         long long current = 1;
-    
         for (int i = 1; i < rowIndex; ++i) {
             current = current * (rowIndex - i + 1) / i;
             row[i] = static_cast<int>(current);
