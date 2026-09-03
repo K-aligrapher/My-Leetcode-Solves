f6a9d1ea9c8028e1ae8c76560ecf8a9dbd8d7288
P Koti Darshan <152671869+K-aligrapher@users.noreply.github.com>
2026-09-03 21:51:00 +0530

    Time: 8 ms (47.38%), Space: 165.8 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+class Solution {
+public:
+    bool uniformArray(std::vector<int>& nums1) {
+        int min_odd = INT_MAX;
+        
+        for (int x : nums1) {
+            if (x % 2 != 0) { 
+                min_odd = min(min_odd, x);
+            }
+        }
+        
+        if (min_odd == INT_MAX) {
+            return true;
+        }
+        
+        for (int x : nums1) {
+            if (x % 2 == 0 && x < min_odd) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
