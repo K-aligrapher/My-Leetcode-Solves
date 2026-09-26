877ad9d1798c75d667ef652fdeeca13fc40acf82
P Koti Darshan <152671869+K-aligrapher@users.noreply.github.com>
2026-09-27 01:11:09 +0530

    Time: 73 ms (76.85%), Space: 122.8 MB (46.61%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        // 1. Build a fast lookup hash map from the knowledge base
+        unordered_map<string, string> dict;
+        for (const auto& pair : knowledge) {
+            dict[pair[0]] = pair[1];
+        }
+        
+        string result = "";
+        string current_key = "";
+        bool inside_bracket = false;
+        
+        // 2. Parse the string in a single pass
+        for (char c : s) {
+            if (c == '(') {
+                inside_bracket = true;
+            } 
+            else if (c == ')') {
+                inside_bracket = false;
+                // Look up the key in the map
+                auto it = dict.find(current_key);
+                if (it != dict.end()) {
+                    result += it->second; // Key found
+                } else {
+                    result += '?';        // Key not found
+                }
+                current_key = ""; // Reset for the next bracket pair
+            } 
+            else {
+                // If we are inside brackets, we are building the key name
+                if (inside_bracket) {
+                    current_key += c;
+                } else {
+                    result += c;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
